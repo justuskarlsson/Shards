3852cf96c69012d82615d7503948df92f55154a3
Justus <juska933@student.liu.se>
2022-08-23 22:45:14 +0200

    Add project files.

---
+++ SliceThrough.cs
@@ -0,0 +1,45 @@
+﻿//using HarmonyLib;
+//using TaleWorlds.MountAndBlade;
+
+//namespace Shards
+//{
+//    [HarmonyPatch]
+//    internal static class SliceThrough
+//    {
+//        [HarmonyPrefix]
+//        [HarmonyPatch(typeof(MissionCombatMechanicsHelper), "GetAttackCollisionResults")]
+//        public static void GetAttackCollisionResults(AttackInformation attackInformation,
+//            ref bool crushedThrough, ref float momentumRemaining, MissionWeapon attackerWeapon,
+//            bool cancelDamage, ref AttackCollisionData attackCollisionData,
+//            out CombatLogData combatLog, out int speedBonus) {
+//            combatLog = new CombatLogData();
+//            speedBonus = 0;
+
+//            //bool is_shard_blade = attackerWeapon.GetModifiedItemName().Contains("Shard Blade");
+//            if (attackInformation.AttackerAgentCharacter.IsHero) {
+//                momentumRemaining = 1000.0f;
+//                crushedThrough = true;
+//                //cancelDamage = false;
+
+//                //BoneBodyPartType hitBodyPart = BoneBodyPartType.Head;
+//                //attackCollisionData = new AttackCollisionData(
+//                //    attackCollisionData.AttackBlockedWithShield, attackCollisionData.CorrectSideShieldBlock, attackCollisionData.IsAlternativeAttack, attackCollisionData.IsColliderAgent, attackCollisionData.CollidedWithShieldOnBack, attackCollisionData.IsMissile,
+//                //    attackCollisionData.MissileBlockedWithWeapon, attackCollisionData.MissileHasPhysics, attackCollisionData.EntityExists, attackCollisionData.ThrustTipHit, attackCollisionData.MissileGoneUnderWater, attackCollisionData.MissileGoneOutOfBorder,
+//                //    attackCollisionData.CollisionResult, attackCollisionData.AffectorWeaponSlotOrMissileIndex, attackCollisionData.StrikeType, attackCollisionData.DamageType, attackCollisionData.CollisionBoneIndex, hitBodyPart,
+//                //    attackCollisionData.AttackBoneIndex, attackCollisionData.AttackDirection, attackCollisionData.PhysicsMaterialIndex, attackCollisionData.CollisionHitResultFlags, attackCollisionData.AttackProgress, attackCollisionData.CollisionDistanceOnWeapon,
+//                //    attackCollisionData.AttackerStunPeriod, attackCollisionData.DefenderStunPeriod, attackCollisionData.MissileTotalDamage, attackCollisionData.MissileStartingBaseSpeed, attackCollisionData.ChargeVelocity, attackCollisionData.FallSpeed,
+//                //    attackCollisionData.WeaponRotUp, attackCollisionData.WeaponBlowDir, attackCollisionData.CollisionGlobalPosition,
+//                //    attackCollisionData.MissileVelocity, attackCollisionData.MissileStartingPosition, attackCollisionData.VictimAgentCurVelocity, attackCollisionData.CollisionGlobalNormal
+//                //    );
+
+//            }
+//            //_attackBlockedWithShield, _correctSideShieldBlock, _isAlternativeAttack, _isColliderAgent, _collidedWithShieldOnBack, _isMissile, _isMissileBlockedWithWeapon, _missileHasPhysics,
+//            //_entityExists, _thrustTipHit, _missileGoneUnderWater, _missileGoneOutOfBorder, collisionResult, affectorWeaponSlotOrMissileIndex, StrikeType, DamageType, CollisionBoneIndex, VictimHitBodyPart,
+//            // AttackBoneIndex, AttackDirection, PhysicsMaterialIndex, CollisionHitResultFlags, AttackProgress, CollisionDistanceOnWeapon, AttackerStunPeriod, DefenderStunPeriod, MissileTotalDamage, MissileInitialSpeed,
+//            // ChargeVelocity, FallSpeed, WeaponRotUp, _weaponBlowDir, CollisionGlobalPosition, MissileVelocity, MissileStartingPosition, VictimAgentCurVelocity, GroundNormal);
+
+
+//        }
+//    }
+
+//}
---
+++ SubModule.cs
@@ -0,0 +1,168 @@
+﻿using TaleWorlds.ObjectSystem;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View.MissionViews;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
+using TaleWorlds.Engine;
+using TaleWorlds.Library;
+using System.Collections.Generic;
+using HarmonyLib;
+using System.Diagnostics;
+
+namespace Shards
+{
+    [HarmonyPatch]
+    public class SubModule : MBSubModuleBase
+    {
+
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            //new Harmony("com.illuvatar.shards").PatchAll(Assembly.GetExecutingAssembly());
+            //InitialStateOption option = new("Message",
+            //    new TextObject("Message", null),
+            //    9990,
+            //    () => {
+            //        InformationManager.DisplayMessage(
+            //            new InformationMessage("Hello World.")
+            //        );
+            //    },
+            //    () => {
+            //        return (false, null);
+            //    }
+
+            //);
+            //Module.CurrentModule.AddInitialStateOption(option);
+
+        }
+
+        //[HarmonyPrefix]
+        //[HarmonyPatch(typeof(MissionCombatMechanicsHelper), "CalculateBaseMeleeBlowMagnitude")]
+        //private static void CalculateBaseMeleeBlowMagnitude(AttackInformation attackInformation, MissionWeapon weapon,
+        //    StrikeType strikeType, float progressEffect, ref float impactPointAsPercent,
+        //    ref float exraLinearSpeed, bool doesAttackerHaveMount) 
+        //{
+        //    Harmony.DEBUG = true;
+        //    List<string> buffer = FileLog.GetBuffer(true);
+
+        //    buffer.Add("");
+        //    buffer.Add("");
+        //    buffer.Add("CalculateBaseMeleeBlowMagnitude");
+        //    buffer.Add("Weapon item name: " + weapon.GetModifiedItemName());
+        //    buffer.Add("Weapon player victim: " + attackInformation.IsVictimPlayer);
+        //    buffer.Add("progressEffect: " + progressEffect);
+        //    buffer.Add("impactPointAsPercent: " + impactPointAsPercent);
+        //    FileLog.LogBuffered(buffer);
+        //    FileLog.FlushBuffer();
+
+        //    Harmony.DEBUG = false;
+        //    if (weapon.GetModifiedItemName().Contains("Shard Blade")) {
+        //        exraLinearSpeed *= 10.0f;
+        //        impactPointAsPercent *= 2f;
+        //    }
+            
+        //}
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MissionCombatMechanicsHelper), "GetAttackCollisionResults")]
+        private static void GetAttackCollisionResults(AttackInformation attackInformation,
+            bool crushedThrough, ref float momentumRemaining, MissionWeapon attackerWeapon,
+            ref bool cancelDamage, ref AttackCollisionData attackCollisionData, CombatLogData combatLog, int speedBonus)
+        {
+            Harmony.DEBUG = true;
+            List<string> buffer = FileLog.GetBuffer(true);
+
+            buffer.Add("");
+            buffer.Add("");
+            buffer.Add("CalculateBaseMeleeBlowMagnitude");
+            buffer.Add("Weapon item name: " + attackerWeapon.GetModifiedItemName());
+            buffer.Add("Weapon player victim: " + attackInformation.IsVictimPlayer);
+            buffer.Add("momentumRemaining: " + momentumRemaining);
+            FileLog.LogBuffered(buffer);
+            FileLog.FlushBuffer();
+
+            Harmony.DEBUG = false;
+            if (attackerWeapon.GetModifiedItemName().Contains("Shard Blade")) {
+                momentumRemaining = 1000f;
+                cancelDamage = false;
+            }
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+            var harmony = new Harmony("com.Shards.jullinator");
+
+            harmony.PatchAll();
+        }
+
+        public override void OnMissionBehaviorInitialize(Mission mission) {
+            base.OnMissionBehaviorInitialize(mission);
+            //mission.AddMissionBehavior(new ShardsMissionBehavior());
+        }
+    }
+
+    //public class ShardsMissionBehavior : MissionView {
+    //    public override void OnMissionScreenInitialize() {
+    //        base.OnMissionScreenInitialize();
+    //        var itemRoster = MobileParty.MainParty.ItemRoster;
+    //        var shardPlate = MBObjectManager.Instance.GetObject<ItemObject>("shard_plate");
+
+    //        //if (itemRoster.)
+    //    }
+
+
+    //    //public override void OnScoreHit(Agent affectedAgent, Agent affectorAgent, WeaponComponentData attackerWeapon, bool isBlocked, bool isSiegeEngineHit, in Blow blow, in AttackCollisionData collisionData, float damagedHp, float hitDistance, float shotDifficulty) {
+    //    //    if (affectorAgent.IsMainAgent) {
+    //    //        affectedAgent.Die(blow, Agent.KillInfo.Backstabbed);
+    //    //    } else {
+    //    //        base.OnScoreHit(affectedAgent, affectorAgent, attackerWeapon, isBlocked, isSiegeEngineHit, blow, collisionData, damagedHp, hitDistance, shotDifficulty);
+    //    //    }
+    //    //}
+
+    //    public override void OnRegisterBlow(Agent attacker, Agent victim, GameEntity realHitEntity, Blow b, ref AttackCollisionData collisionData, in MissionWeapon attackerWeapon) {
+    //        if (attacker.IsMainAgent) {
+    //            victim.Die(b, Agent.KillInfo.Backstabbed);
+    //            //Mission.MainAgent.WieldedWeapon
+    //            b.BaseMagnitude = 5000f;
+                
+    //            collisionData.BaseMagnitude = 5000f;
+    //            //b.WeaponRecord.Velocity *= 2000f;
+    //            b.SwingDirection *= 100;
+    //        } else {
+    //            base.OnRegisterBlow(attacker, victim, realHitEntity, b, ref collisionData, attackerWeapon);
+    //        }
+    //    }
+
+    //}
+
+    public class ShardsCombatBehavior : MissionMainAgentController
+    {
+    }
+
+
+    //public class ShardsBehaviour : CampaignBehaviorBase
+    //{
+    //    public override void RegisterEvents() {
+    //        CampaignEvents.BeforeMissionOpenedEvent.AddNonSerializedListener(this, OnMissionOpened);
+    //        //CampaignEvents.OnMissionStartedEvent.AddNonSerializedListener(this,)
+    //    }
+
+    //    public void OnMissionOpened() {
+    //        Campaign.Current.SkillLevelingManager
+    //    }
+
+    //    public override void SyncData(IDataStore dataStore) {
+    //        throw new System.NotImplementedException();
+    //    }
+    //}
+}
