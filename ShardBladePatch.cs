f57e9d3aa28fa83bf502bd6eb2ff9326cd466a27
Justus Karlsson <justus.karlsson@hotmail.se>
2022-08-24 22:43:43 +0200

    Cleaned up code and split into multiple files.

---
+++ ShardBladePatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace Shards
+{
+    [HarmonyPatch]
+    internal static class SliceThrough
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MissionCombatMechanicsHelper), "GetAttackCollisionResults")]
+        private static void GetAttackCollisionResults(AttackInformation attackInformation,
+            ref bool crushedThrough, ref float momentumRemaining, MissionWeapon attackerWeapon,
+            ref bool cancelDamage, ref AttackCollisionData attackCollisionData, 
+            ref CombatLogData combatLog, ref int speedBonus) 
+        {
+
+            if (attackerWeapon.GetModifiedItemName().Contains("Shard Blade")) {
+                momentumRemaining = 10f;
+                crushedThrough = true;
+                cancelDamage = false;
+            }
+        }
+
+    }
+
+}
